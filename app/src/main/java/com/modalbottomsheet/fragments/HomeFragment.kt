1e8cfe84a83e9720580cc159a7f9c5f930a68020
arsildo <arsildo.murati@gmail.com>
2020-11-21 16:01:05 +0100

    Initial commit

---
+++ app/src/main/java/com/modalbottomsheet/fragments/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.modalbottomsheet.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.modalbottomsheet.R
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_bottomSheetFragment)
+        }
+
+    }
+}
